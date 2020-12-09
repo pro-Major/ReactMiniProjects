849d2a06ae3d188cef4a9ab4a59401757a700383
Major <navikbipin@gmail.com>
2020-12-09 23:34:01 +0530

    Project5 Added

---
+++ project5/src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const App = () => {
+   return(
+        <React.Fragment>
+    <h1> Hello! have a nice Day.</h1>
+        </React.Fragment>
+    )
+}
+
+export default App;
---
+++ project5/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom, { render } from "react-dom";
+import App from "./app";
+import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+ReactDom.render(<App/>,document.getElementById('root'));
+
+
