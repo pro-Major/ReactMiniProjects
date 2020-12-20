ad4a7859e3a0b89268f4255516249d12ab2a1f98
Major <navikbipin@gmail.com>
2020-12-20 10:31:40 +0530

    proj3 all files added

---
+++ project3/src/app.jsx
@@ -0,0 +1,17 @@
+import React,{Component} from "react";
+import axios from "axios";
+
+
+class App extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        
+
+        return(
+            <h1> Hello genius </h1>
+         )
+    }
+}
+export default App;
