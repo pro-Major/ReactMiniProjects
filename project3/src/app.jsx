2b26b6e520eb7bfc2a483991ec03d64f05fec68c
Major <navikbipin@gmail.com>
2020-12-22 22:26:51 +0530

    project 2 update

--- project3/src/app.jsx
+++ project3/src/app.jsx
@@ -1,11 +1,20 @@
 import React,{Component} from "react";
-import axios from "axios";
+import {axios} from "axios";
 
 
 class App extends Component{
     constructor(){
-        super()
+        super();
+        this.state={
+            ProductsData : [ ],
+            SearchedData: ''
+        }
     }
+    fetchData = async () => {
+        let url = "https://pixabay.com/api/?key=19432804-bd5acf9b634cc280bbdda0f8f&q=yellow+flowers&image_type=photo&pretty=true";
+        let response = await axios.get(url)  //method="" /* get, delete, head, post, put and patch - required */
+
+        }
     render(){
         
 
