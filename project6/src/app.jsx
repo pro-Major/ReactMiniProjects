edaec49260ed67375814e15db7136fff55865472
Major <navikbipin@gmail.com>
2020-12-22 22:49:02 +0530

    projec6 hello world

---
+++ project6/src/app.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+import ReactDom from "react-dom";
+
+class App extends Component{
+    render(){
+        return (
+            <h1> Hemllo Cheems</h1>
+        )
+    }
+}
+export default App;
