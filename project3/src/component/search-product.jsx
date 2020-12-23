3bf8537331cde5f04e85bd17cc8fb026bf8b4554
Major <navikbipin@gmail.com>
2020-12-23 23:10:07 +0530

    recreation of project3

--- project3/src/component/search-product.jsx
+++
@@ -1,9 +0,0 @@
-import React,{Component} from "react";
-class SearchProduct extends Component{
-    render() {
-        return(
-            <h1>Hemlo Cheems </h1> 
-        )
-    }
-}
-export default SearchProduct;
