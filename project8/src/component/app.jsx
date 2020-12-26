cea5748d3e9e4236a2c0d2f6d40d02ef24889175
Major <navikbipin@gmail.com>
2020-12-26 17:11:12 +0530

    app and index added

---
+++ project8/src/component/app.jsx
@@ -0,0 +1,18 @@
+import React,{Component} from 'react'
+class App extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <React.Fragment>
+                <div className="container">
+                <h1>Hello Major</h1> 
+                </div>
+             
+            </React.Fragment>
+           
+        )
+    }
+}
+export default App;
--- project8/src/index.js
+++ project8/src/index.js
@@ -1,2 +1,6 @@
 import React from 'react';
-  
+import ReactDOM from 'react-dom';
+import App from './component/app'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(<App/>,document.getElementById('root'));
