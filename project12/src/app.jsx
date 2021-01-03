bc24e2981f98f17ed544fca3f9c21b1a97692933
Major <navikbipin@gmail.com>
2021-01-03 19:12:56 +0530

    Project 12 Added

---
+++ project12/src/app.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class App extends Component{
+    render(){
+        return(
+            <h1> Project 11 Hight Order of Component </h1>
+        )
+    }
+}
+export default App;
