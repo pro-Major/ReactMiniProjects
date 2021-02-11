0035ef43f75ba9424677919bb2f0b70c7593e30e
Major <navikbipin@gmail.com>
2021-02-11 22:20:13 +0530

    Couting Project Renamed

---
+++ Simple_Counting_Button/src/app.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Count from "./count";
+//useState (ReactHook) must be defined inside of top level function
+const App=()=>{
+    const state = useState();
+    // console.log(state)
+    const [count,setCount] = useState(1); //useerState me initial value hai count me current value hai setcount me update value hai 
+    
+    const changethenumber=()=>{
+        
+        setCount(count + 1); 
+        // console.log(countednumber)
+    };
+    return(
+        <React.Fragment>
+            
+          <div className="container"  style={{
+          backgroundColor:"skyblue",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+           height: "650px",
+           border : "1px solid black " 
+         
+        }}>
+            <div className="column" >
+                <div className="container" style={{ }}>
+                <h1> {count}</h1>
+                </div>
+            {/* <Count count={state.count}/> */} 
+            <button className="btn btn-danger" onClick={changethenumber}> Click Me </button>
+            </div>
+            </div>
+            
+            
+        </React.Fragment>
+    )
+}
+export default App;
